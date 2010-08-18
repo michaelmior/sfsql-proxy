d63e268f1ca2777ecdea618f0c4dc0dc11c3c6ff
Michael Mior <mmior@cs.toronto.edu>
2010-08-18 10:49:34 -0400

    Copied license back to sql_string.c with explanatory comment.

--- sql_string.c
+++ sql_string.c
@@ -1,11 +1,34 @@
+/* Copyright (C) 2000 MySQL AB
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+/* This file is originally from the mysql distribution. Coded by monty */
+
+/* Parts of this code relying on C++ have been stripped.
+ * 
+ * The only function currently used is copy_and_convert()
+ * during client authentication
+ *
+ * Michael Mior <mmior@cs.toronto.edu>
+ */
+
 #include <my_global.h>
 #include <m_ctype.h>
 #include <m_string.h>
 #include <my_sys.h>
 
 #include <string.h>
-
-/* copied from client/sql_string.c */
 
 /****************************************************************************
   Help functions
