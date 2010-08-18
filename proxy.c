733eaffed12c5e9c7748801d74404cd87c01797c
Michael Mior <mmior@cs.toronto.edu>
2010-08-18 14:45:55 -0400

    Added command-line options for specifying connection info

--- proxy.c
+++ proxy.c
@@ -4,6 +4,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <signal.h>
+#include <getopt.h>
 
 #include "proxy.h"
 
@@ -22,7 +23,7 @@ void proxy_error(const char *fmt, ...) {
     fprintf(stderr, "\n");
 }
 
-void server_run() {
+void server_run(int port) {
     int serverfd, clientfd, optval, error;
     fd_set fds;
     unsigned int clientlen;
@@ -39,17 +40,23 @@ void server_run() {
 #endif
 
     /* Intialize the server address */
+    port = port < 0 ? PROXY_PORT : port;
+
     memset(&serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); //TODO: allow specification of binding address
-    serveraddr.sin_port = htons((unsigned short) PROXY_PORT);
+    serveraddr.sin_port = htons((unsigned short) port);
 
     /* Bind the socket and start accepting connections */
-    if (bind(serverfd, (struct sockaddr*) &serveraddr, sizeof(serveraddr)) < 0)
-        proxy_error("Error binding server socket on port %d", PROXY_PORT);
+    if (bind(serverfd, (struct sockaddr*) &serveraddr, sizeof(serveraddr)) < 0) {
+        proxy_error("Error binding server socket on port %d", port);
+        return;
+    }
 
-    if (listen(serverfd, QUEUE_LENGTH) < 0)
+    if (listen(serverfd, QUEUE_LENGTH) < 0) {
         proxy_error("Error listening on server socket: %s", strerror(errno));
+        return;
+    }
 
     /* Server event loop */
     clientlen = sizeof(clientaddr);
@@ -110,8 +117,69 @@ void catch_sig(int sig) {
     }
 }
 
+void usage() {
+    printf(
+            "SnowFlock SQL proxy server - (C) Michael Mior <mmior@cs.toronto.edu>, 2010\n\n"
+            "Options:\n"
+            "\t--backend-host, -h\tHost to forward queries to\n"
+            "\t--backend-port, -p\tPort of the backend host\n"
+            "\t--backend-db,   -D\tName of database on the backend\n"
+            "\t--backend-user, -u\tUser for backend server\n"
+            "\t--backend-pass, -p\tPassword for backend user\n\n"
+            "\t--proxy-port, -L\tPort for the proxy server to listen on\n"
+    );
+}
+
 int main(int argc, char *argv[]) {
-    int error;
+    int error, bport, pport, c;
+    char *host, *db, *user, *pass;
+
+    /* Set arguments to default values */
+    bport = pport = -1;
+    host = db = user = pass = NULL;
+
+    /* Parse command-line options */
+    while(1) {
+        static struct option long_options[] = {
+            {"backend-host", optional_argument, 0, 'h'},
+            {"backend-port", optional_argument, 0, 'P'},
+            {"backend-db",   optional_argument, 0, 'D'},
+            {"backend-user", optional_argument, 0, 'u'},
+            {"backend-pass", optional_argument, 0, 'p'},
+            {"proxy-port",   optional_argument, 0, 'L'},
+            {0, 0, 0, 0}
+        };
+
+        int opt = 0;
+        c = getopt_long(argc, argv, "h:P:D:u:p:L:", long_options, &opt);
+
+        if (c == -1)
+            break;
+
+        switch(c) {
+            case 'h':
+                host = strdup(optarg);
+                break;
+            case 'P':
+                bport = atoi(optarg);
+                break;
+            case 'D':
+                db = strdup(optarg);
+                break;
+            case 'u':
+                user = strdup(optarg);
+                break;
+            case 'p':
+                pass = strdup(optarg);
+                break;
+            case 'L':
+                pport = atoi(optarg);
+                break;
+            default:
+                usage();
+                return EXIT_FAILURE;
+        }
+    }
 
     /* Install signal handler */
     signal(SIGINT, catch_sig);
@@ -130,11 +198,12 @@ int main(int argc, char *argv[]) {
     mysql->server_version   = MYSQL_SERVER_VERSION;
 
     /* Connect to the backend server (default parameters for now) */
-    if ((error = proxy_backend_connect(NULL, -1, NULL, NULL, NULL)))
+    if ((error = proxy_backend_connect(host, bport, user, pass, db)))
         goto out;
 
     /* Start proxying */
-    server_run();
+    printf("Staring proxy on port %d\n", pport);
+    server_run(pport);
 
     /* Shutdown */
 out:
