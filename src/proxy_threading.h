51afd66f9fcdf87c014cb979146f409779ea87fb
Michael Mior <mmior@cs.toronto.edu>
2010-11-01 12:47:12 -0400

    Simplified errstr macro.

--- src/proxy_threading.h
+++ src/proxy_threading.h
@@ -61,13 +61,7 @@ typedef struct {
 pthread_key_t thread_buf_key;
 
 /** Get an error message in a thread-safe way using thread-specific data as a buffer */
-#define errstr \
-    ({ \
-        char *_buf = pthread_getspecific(thread_buf_key); \
-        if (!_buf || strerror_r(errno, _buf, BUFSIZ)) \
-            _buf = ""; \
-        _buf; \
-     })
+#define errstr strerror_r(errno, pthread_getspecific(thread_buf_key), BUFSIZ) ?: ""
 
 void proxy_threading_init();
 void proxy_threading_mask();
